d5aa6edec7cff4d77dcf070b5da226766339175c
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2023-03-24 20:23:33 +0100

    not passing 2

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+   this.items.push(item); 
+   this.items.sort((a, b) => a-b ); 
+   this.length = this.items.length; 
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[pos];   
+  }
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+return this.items[this.items.length - 1]
+}
+    }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+  } else {
+ return this.items[0];
+  }
+  }
 
-  max() {}
+  sum() {
+    let result = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      return result += this.items[i];
+    }
+     
+  
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+    if (!this.items.length) {
+      return 0;
+    }
+  }
+  avg() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
